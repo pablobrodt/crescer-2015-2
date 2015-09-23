c26d6059dd5cc3cf83f0aaaa64e7ac937928f0d4
Pablo brodt <pablobrodt@gmail.com>
2015-09-23 00:24:35 -0300

    test(UrukHaiTest): adicionar testes

--- src/modulo-01-java-OO/Modulo01/UrukHaiTest.java
+++ src/modulo-01-java-OO/Modulo01/UrukHaiTest.java
@@ -20,4 +20,24 @@ public class UrukHaiTest
         
         assertEquals(esperado, orc.getInventario());
     }
+    
+    @Test
+    public void urukHaiAtacaSnaga(){
+        Snaga orc = new Snaga();
+        UrukHai uruk = new UrukHai();
+        
+        orc.atacar(uruk);
+        
+        assertEquals(144, uruk.getVida());
+    }
+    
+    @Test
+    public void urukHaiAtacaUrukHai(){
+        UrukHai orc = new UrukHai();
+        UrukHai orcAlvo = new UrukHai();
+        
+        orc.atacar(orcAlvo);
+        
+        assertEquals(144, orcAlvo.getVida());
+    }
 }
