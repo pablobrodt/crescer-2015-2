f2295de35a92cb5347b5253105fc95f522ca6f88
Pablo brodt <pablobrodt@gmail.com>
2015-09-23 21:45:47 -0300

    feat(ExercitoDeElfos): adicionar metodo

--- src/modulo-01-java-OO/Modulo01/ExercitoDeElfos.java
+++ src/modulo-01-java-OO/Modulo01/ExercitoDeElfos.java
@@ -1,8 +1,10 @@
 import java.util.HashMap;
+import java.util.ArrayList;
 
 public class ExercitoDeElfos
 {
     private HashMap<String, Elfo> exercito = new HashMap<>();
+    private HashMap<Status, ArrayList<Elfo>> exercitoPorStatus;
     
     public void alistarElfo(ElfoVerde elfo){
         this.exercito.put(elfo.getNome(), elfo);
@@ -18,6 +20,20 @@ public class ExercitoDeElfos
     
     public HashMap<String, Elfo> getExercito(){
         return this.exercito;
+    }
+    
+    public HashMap<Status, ArrayList<Elfo>> getExercitoPorStatus(){
+        return this.exercitoPorStatus;
+    }
+    
+    public void agruparPorStatus(){
+        exercitoPorStatus = new HashMap<>();
+        for(Elfo elfo : this.exercito.values()){
+            if( !exercitoPorStatus.containsKey(elfo.getStatus())){
+                exercitoPorStatus.put(elfo.getStatus(), new ArrayList<Elfo>());
+            }
+            exercitoPorStatus.get(elfo.getStatus()).add(elfo);
+        }
     }
     
     public boolean equals(Object obj) {
--- src/modulo-01-java-OO/Modulo01/ExercitoDeElfosTest.java
+++ src/modulo-01-java-OO/Modulo01/ExercitoDeElfosTest.java
@@ -3,6 +3,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.ArrayList;
+
 public class ExercitoDeElfosTest
 {
     /**
@@ -121,5 +124,50 @@ public class ExercitoDeElfosTest
        assertEquals(exercitoEsperado, exercito);
        
    }
-    
+   
+   /**
+    * Ordenar Exercito com 3 Elfos mortos e 3 Elfos vivos
+    */
+    @Test
+    public void exercitoCom3ElfosVivosE3ElfosMortosOrdenado(){
+        ExercitoDeElfos exercito = new ExercitoDeElfos();
+        
+        exercito.alistarElfo(new ElfoVerde("v1"));
+        exercito.alistarElfo(new ElfoVerde("v2"));
+        exercito.alistarElfo(new ElfoVerde("v3"));
+        
+        ElfoNoturno morto1 = new ElfoNoturno("n1");
+        ElfoNoturno morto2 = new ElfoNoturno("n2");
+        ElfoNoturno morto3 = new ElfoNoturno("n3");
+        
+        while(morto1.getStatus() != Status.MORTO && morto2.getStatus() != Status.MORTO && morto2.getStatus() != Status.MORTO ){
+            morto1.atirarFlecha(new Dwarf());
+            morto2.atirarFlecha(new Dwarf());
+            morto3.atirarFlecha(new Dwarf());
+        }
+        
+        exercito.alistarElfo(morto1);
+        exercito.alistarElfo(morto2);
+        exercito.alistarElfo(morto3);
+        
+        HashMap<Status, ArrayList<Elfo>> esperado = new HashMap<>();
+        ArrayList<Elfo> vivos = new ArrayList<>();
+        ArrayList<Elfo> mortos = new ArrayList<>();
+        
+        vivos.add(new ElfoVerde("v1"));
+        vivos.add(new ElfoNoturno("v2"));
+        vivos.add(new ElfoVerde("v3"));
+        
+        mortos.add(morto1);
+        mortos.add(morto2);
+        mortos.add(morto3);
+        
+        esperado.put(Status.VIVO, vivos);
+        esperado.put(Status.MORTO, mortos);
+        
+        exercito.agruparPorStatus();
+        
+        assertEquals(esperado, exercito.getExercitoPorStatus());
+        
+    }
 }
