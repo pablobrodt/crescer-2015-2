7f7a78a272fe230f23ad27f937dccfca85266273
Pablo Brodt <pablobrodt@gmail.com>
2015-10-01 15:19:27 -0300

    Exercicios 1 a 12

--- slides/modulo-02-banco/Exercicios/ExerciciosAula3.sql
+++ slides/modulo-02-banco/Exercicios/ExerciciosAula3.sql
@@ -1,12 +1,12 @@
---Faça uma consulta (query) que retorne apenas o primeiro nome do Associado (tabela associado).
+--1)Faça uma consulta (query) que retorne apenas o primeiro nome do Associado (tabela associado).
 SELECT SUBSTRING(Nome, 0, CHARINDEX(' ', Nome))
 FROM Associado;
 
---Faça uma consulta que retorne o nome dos associados e a idade de cada um (em anos, exibir um número inteiro).
+--2)Faça uma consulta que retorne o nome dos associados e a idade de cada um (em anos, exibir um número inteiro).
 SELECT Nome, DATEDIFF(year, DataNascimento, getdate()) as Idade
 FROM Associado;
 
---Faça uma consulta que liste os empregados admitidos entre 01/05/1980 e 20/01/1982. 
+--3)Faça uma consulta que liste os empregados admitidos entre 01/05/1980 e 20/01/1982. 
 --Exibir também o total de meses de trabalho até a data de 31/12/2000.
 SELECT 
 	IDEmpregado, 
@@ -21,7 +21,7 @@ SELECT
 FROM Empregado
 WHERE DataAdmissao BETWEEN '1980/05/01' AND '1982/12/31';
 
---Qual o cargo (tabela empregado) possuir mais empregados?
+--4)Qual o cargo (tabela empregado) possuir mais empregados?
 SELECT TOP 1 COUNT(1) as Quantidade, Cargo
 FROM Empregado
 GROUP BY Cargo
@@ -30,18 +30,18 @@ ORDER BY COUNT(1) DESC
 
 
 
---Qual é o associado de maior nome (número de caracteres) ?
+--5)Qual é o associado de maior nome (número de caracteres) ?
 SELECT TOP 1 Nome, LEN(Nome)
 FROM Associado
 ORDER BY LEN(Nome) DESC 
 
 
---Faça uma consulta que retorne o nome do associado e a data de quando cada completará 
+--6)Faça uma consulta que retorne o nome do associado e a data de quando cada completará 
 --(ou completou) 50 anos, liste também o dia da semana.
 SELECT Nome, DATEADD(year, 50, DataNascimento) Aniversario50, DATENAME(weekday, DATEADD(year, 50, DataNascimento))
 FROM Associado;
 
---Liste a quantidade de cidades agrupando por UF.
+--7)Liste a quantidade de cidades agrupando por UF.
 SELECT COUNT(1), UF
 FROM Cidade
 GROUP BY UF;
@@ -53,9 +53,32 @@ GROUP BY Nome, UF
 HAVING COUNT(Nome) > 1;
 
 --9)Identifique qual deve ser o próximo ID para a criação de um novo registro na tabela Associado (maior + 1).
-SELECT MAX(IDAssociado) + 1
+SELECT ISNULL( MAX(IDAssociado), 0) + 1
 FROM Associado
 
 --10)Limpe a tabela CidadeAux, e insira somente as cidades com nomes e UF’s distintos, 
 --considere somente o menor código ID das cidades duplicadas.
 --TRUNCATE TABLE CidadeAux;
+INSERT INTO CidadeAux(IDCidade, Nome, UF)
+	SELECT MIN(IDCidade) IDCidade, Nome, UF
+	FROM Cidade
+	GROUP BY Nome, UF
+	ORDER BY IDCidade
+
+
+--11)Altere todas cidades duplicadas (nome e uf iguais), acrescente no ínicio do nome um asterisco (*).
+BEGIN TRANSACTION
+UPDATE Cidade
+SET Nome = '*' + Nome
+WHERE Nome IN(
+	SELECT Nome
+	FROM Cidade
+	GROUP BY Nome, UF
+	HAVING COUNT(Nome)>1
+)
+
+
+
+
+--12)Faça uma consulta que liste o nome do Associado e a descrição da coluna Sexo, 
+--informando: Masculino ou Feminino.
