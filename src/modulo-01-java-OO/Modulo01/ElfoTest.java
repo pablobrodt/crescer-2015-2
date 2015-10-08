d70c89e63245b0f373bf67f4faf5d74d17454f05
Pablo Brodt <pablobrodt@gmail.com>
2015-10-08 09:40:29 -0300

    Merge

--- src/modulo-01-java-OO/Modulo01/ElfoTest.java
+++ src/modulo-01-java-OO/Modulo01/ElfoTest.java
@@ -18,6 +18,23 @@ public class ElfoTest
     }
 
     @Test
+    public void elfoNasceVivo() {
+        Elfo legolas = new Elfo("Legolas");
+        assertEquals(Status.VIVO, legolas.getStatus());
+    }
+
+    @Test
+    public void elfoNasceCom100DeVida() {
+        assertEquals(100.0, new Elfo("Legolas").getVida(), 0.0);
+    }
+
+    @Test
+    public void elfoNasceComInventarioVazio() {
+        Elfo legolas = new Elfo("Legolas");
+        assertEquals(new Inventario(), legolas.getInventario());
+    }
+
+    @Test
     public void elfoCriadoNasceCom0DeExperiencia() {
         Elfo legolas = new Elfo("Legolas II");
         assertEquals(0, legolas.getExperiencia());
@@ -57,13 +74,13 @@ public class ElfoTest
         Dwarf balin = new Dwarf();
         int qtdFlechasEsperada = 41;
         int experienciaEsperada = 1;
-        int vidaEsperada = 100;
+        double vidaEsperada = 100;
         // Act
         elfo.atirarFlecha(balin);
         // Assert
         assertEquals(qtdFlechasEsperada, elfo.getFlechas());
         assertEquals(experienciaEsperada, elfo.getExperiencia());
-        assertEquals(vidaEsperada, balin.getVida());
+        assertEquals(vidaEsperada, balin.getVida(), 0.0);
     }
 
     @Test
@@ -74,15 +91,15 @@ public class ElfoTest
         Dwarf filhoDeBalin = new Dwarf();
         int qtdFlechasEsperada = 40;
         int experienciaEsperada = 2;
-        int vidaEsperada = 100;
+        double vidaEsperada = 100;
         // Act
         elfo.atirarFlecha(balin);
         elfo.atirarFlecha(filhoDeBalin);
         // Assert
         assertEquals(qtdFlechasEsperada, elfo.getFlechas());
         assertEquals(experienciaEsperada, elfo.getExperiencia());
-        assertEquals(vidaEsperada, balin.getVida());
-        assertEquals(vidaEsperada, filhoDeBalin.getVida());
+        assertEquals(vidaEsperada, balin.getVida(), 0.0);
+        assertEquals(vidaEsperada, filhoDeBalin.getVida(), 0.0);
     }
 
     @Test
@@ -94,7 +111,7 @@ public class ElfoTest
         Dwarf filhoDeBalin = new Dwarf();
         int qtdFlechasEsperada = 40;
         int experienciaEsperada = 2;
-        int vidaEsperada = 90;
+        double vidaEsperada = 90;
         // Act
         elfo1.atirarFlecha(balin);
         elfo1.atirarFlecha(filhoDeBalin);
@@ -105,8 +122,80 @@ public class ElfoTest
         assertEquals(experienciaEsperada, elfo1.getExperiencia());
         assertEquals(qtdFlechasEsperada, elfo2.getFlechas());
         assertEquals(experienciaEsperada, elfo2.getExperiencia());
-        assertEquals(vidaEsperada, balin.getVida());
-        assertEquals(vidaEsperada, filhoDeBalin.getVida());
+        assertEquals(vidaEsperada, balin.getVida(), 0.0);
+        assertEquals(vidaEsperada, filhoDeBalin.getVida(), 0.0);
     }
 
+    @Test
+    public void elfoComNomeEFlechasInformadasToString() {
+        Elfo elfo1 = new Elfo("Acabaram os nomes", 1000);
+        String textoEsperado = "Acabaram os nomes possui 1000 flechas e 0 níveis de experiência.";
+
+        assertEquals(textoEsperado, elfo1.toString());
+    }
+
+    @Test
+    public void elfoComUmaFlechaInformadaToString() {
+        Elfo elfo1 = new Elfo("Monoflecha", 1);
+        String textoEsperado = "Monoflecha possui 1 flecha e 0 níveis de experiência.";
+        assertEquals(textoEsperado, elfo1.toString());
+    }
+
+    @Test
+    public void elfoComUmDeExperienciaToString() {
+        Elfo elfo1 = new Elfo(null);
+        elfo1.atirarFlecha(new Dwarf());
+        String textoEsperado = "null possui 41 flechas e 1 nível de experiência.";
+        assertEquals(textoEsperado, elfo1.toString());
+    }
+
+    @Test
+    public void elfoRecebeEspadadaDoOrcUrukHai(){
+        Orc orc = new UrukHai();
+        Elfo elfo = new Elfo(null);
+
+        orc.atacar(elfo);
+
+        assertEquals(88, elfo.getVida(), 0.0);
+    }
+
+    @Test
+    public void elfoRecebeFlechadaDoOrcSnaga(){
+        Orc orc = new Snaga();
+        Elfo elfo = new Elfo(null);
+
+        orc.atacar(elfo);
+
+        assertEquals(92, elfo.getVida(), 0.0);
+    }
+
+    @Test
+    public void quandoCriaUmElfoIncrementaQuantidadeDeCriados() {
+        int esperado = 1;
+        new Elfo("Qualquer");
+        assertEquals(esperado, Elfo.getQtdElfosCriados());
+    }
+
+    @Test
+    public void quandoCriaMuitosElfos() {
+        // ArrayList<Elfo> fixture = MontadorDeCenario.meVeAi5ElfosVerdes();
+        
+        int esperado = 100;
+        for (int i = 0; i < 100; i++) {
+            new Elfo("Qualquer: " + i);
+        }
+        assertEquals(esperado, Elfo.getQtdElfosCriados());
+    }
+    
+    @Before
+    public void setUp() {
+        Elfo.resetaContador();
+    }
+    
+    /*
+    @After
+    public void tearDown() {
+        Elfo.resetaContador();
+    }
+    */
 }
