73cfd4105fe4aac44b96d276994020aec3e4528a
Pablo Brodt <pablobrodt@gmail.com>
2015-10-17 11:35:35 -0300

    feat(Exercicios) adicionar função adicionar função maiorTexto

--- src/Web/Javascript/Exercicios/lista-js-basico.js
+++ src/Web/Javascript/Exercicios/lista-js-basico.js
@@ -1,10 +1,30 @@
 
-//Exercicio 1
-
+/*Exercicio 1
+	Faça uma função chamada daisyGame que receba por argumento o número de pétalas 
+	da margarida e retorne 'Love me' ou 'Love me not' (Lembre do jogo "Bem me quer, 
+	mal me quer").
+*/
 function daisyGame( numeroPetalas ){
 	var resultado = ( numeroPetalas % 2 === 0 ) ? 'Love me not' : 'Love me';
 	return resultado;
 }
 
+
+
+/*Exercicio 2
+	Faça uma função chamada maiorTexto que receba um array de strings e retorne o texto 
+	com maior número de caracteres.
+*/
+function maiorTexto( arrai ){
+	var maiotTexto = arrai[0];
+
+	for ( texto of arrai ){
+		if( texto.length > maiorTexto.length ){
+			maiorTexto = texto;
+		}
+	}
+
+	return maiorTexto;
+}
 
 
