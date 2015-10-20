93f314171a91af55c3d4d9097401018e75f229eb
Pablo Brodt <pablobrodt@gmail.com>
2015-10-20 17:24:00 -0200

    fix(Exercicios): correção de classes

--- src/modulo-03-web/dia-03/Exercicios/CarrinhoDeCompras.js
+++ src/modulo-03-web/dia-03/Exercicios/CarrinhoDeCompras.js
@@ -1,3 +1,3 @@
 function CarrinhoDeCompras(){
-	var this.itens = [];
+	this.itens = [];
 }
--- src/modulo-03-web/dia-03/Exercicios/Item.js
+++ src/modulo-03-web/dia-03/Exercicios/Item.js
@@ -1,6 +1,6 @@
 function Item(sku, descricao, quantidade, valorUnitario){
-	var this.sku = sku;
-	var this.descricao = descricao;
-	var this.quantidade = quantidade;
-	var this.valorUnitario = valorUnitario;
+	this.sku = sku;
+	this.descricao = descricao;
+	this.quantidade = quantidade;
+	this.valorUnitario = valorUnitario;
 }
