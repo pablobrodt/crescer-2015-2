a9ba6b05c729e48756c1b97244ff8193941cdb67
Pablo Brodt <pablobrodt@gmail.com>
2015-10-20 16:45:29 -0200

    feat(colecoes): adicionar função

--- src/modulo-03-web/dia-01/Exercicios/colecoes.js
+++ src/modulo-03-web/dia-01/Exercicios/colecoes.js
@@ -1,6 +1,7 @@
 var clubes = [
   {
     nome: 'Arsenal',
+    fundacao: new Date(1886, 0, 1),
     titulos: [
       { desc: 'Nacionais', qtd: 13 },
       { desc: 'Continentais', qtd: 0 },
@@ -9,6 +10,7 @@ var clubes = [
   }, 
   {
     nome: 'Manchester United',
+    fundacao: new Date(1878, 0, 1),
     titulos: [
       { desc: 'Nacionais', qtd: 20 },
       { desc: 'Continentais', qtd: 3 },
@@ -17,6 +19,7 @@ var clubes = [
   },
   {
     nome: 'Liverpool',
+    fundacao: new Date(1892, 2, 15),
     titulos: [
       { desc: 'Nacionais', qtd: 18 },
       { desc: 'Continentais', qtd: 5 },
@@ -25,6 +28,7 @@ var clubes = [
   },
   {
     nome: 'Chelsea Football Club',
+    fundacao: new Date(1905, 2, 10),
     titulos: [
       { desc: 'Nacionais', qtd: 5 },
       { desc: 'Continentais', qtd: 1 },
@@ -177,3 +181,18 @@ var clubes = [
   });
 
  }
+
+
+//exercicio 4
+
+function calcularIdadeMedia( clubes ){
+
+    var soma = clubes.reduce(function(acumulador, clube){
+
+      return acumulador + (new Date().getFullYear() - clube.fundacao.getFullYear());
+
+    }, 0);
+
+    return soma / 4;
+
+}
