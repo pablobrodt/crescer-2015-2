251d74e0398cc0a1b0bd5592b2184add42db4b5e
Pablo Brodt <pablobrodt@gmail.com>
2015-10-26 20:44:24 -0200

    feat(AgendaTeste): adicionar teste

--- src/modulo-04-c-sharp/dia-01/Exercicio/ConsoleAppTestes/AgendaTeste.cs
+++ src/modulo-04-c-sharp/dia-01/Exercicio/ConsoleAppTestes/AgendaTeste.cs
@@ -62,6 +62,55 @@ namespace ConsoleAppTestes
         }
 
         [TestMethod]
+        public void AgendaListaDozeContatosOrdenadosPorNome()
+        {
+            string resultadoEsperado = "Nome: Afrodite de Peixes - Numero: 12\n";
+            resultadoEsperado += "Nome: Aiolia de Leão - Numero: 5\n";
+            resultadoEsperado += "Nome: Aiolos de Sagitário - Numero: 9\n";
+            resultadoEsperado += "Nome: Aldebaran de Touro - Numero: 2\n";
+            resultadoEsperado += "Nome: Camus de Aquário - Numero: 11\n";
+            resultadoEsperado += "Nome: Dohko de Libra - Numero: 7\n";
+            resultadoEsperado += "Nome: Máscara da Morte de Câncer - Numero: 4\n";
+            resultadoEsperado += "Nome: Milo de Escorpião - Numero: 8\n";
+            resultadoEsperado += "Nome: Mu de Áries - Numero: 1\n";
+            resultadoEsperado += "Nome: Saga de Gêmeos - Numero: 3\n";
+            resultadoEsperado += "Nome: Shaka de Virgem - Numero: 6\n";
+            resultadoEsperado += "Nome: Shura de Capricórnio - Numero: 10\n";
+            
+            var agenda = new Agenda();
+
+            var contato1 = new Contato("Mu de Áries", 1);
+            var contato2 = new Contato("Aldebaran de Touro", 2);
+            var contato3 = new Contato("Saga de Gêmeos", 3);
+            var contato4 = new Contato("Máscara da Morte de Câncer", 4);
+            var contato5 = new Contato("Aiolia de Leão", 5);
+            var contato6 = new Contato("Shaka de Virgem", 6);
+            var contato7 = new Contato("Dohko de Libra", 7);
+            var contato8 = new Contato("Milo de Escorpião", 8);
+            var contato9 = new Contato("Aiolos de Sagitário", 9);
+            var contato10 = new Contato("Shura de Capricórnio", 10);
+            var contato11 = new Contato("Camus de Aquário", 11);
+            var contato12 = new Contato("Afrodite de Peixes", 12);
+
+            agenda.AdicionarContato(contato1);
+            agenda.AdicionarContato(contato2);
+            agenda.AdicionarContato(contato3);
+            agenda.AdicionarContato(contato4);
+            agenda.AdicionarContato(contato5);
+            agenda.AdicionarContato(contato6);
+            agenda.AdicionarContato(contato7);
+            agenda.AdicionarContato(contato8);
+            agenda.AdicionarContato(contato9);
+            agenda.AdicionarContato(contato10);
+            agenda.AdicionarContato(contato11);
+            agenda.AdicionarContato(contato12);
+
+            string contatos = agenda.ListarContatosOrdenadoPorNome();
+
+            Assert.AreEqual(contatos, resultadoEsperado);
+        }
+
+        [TestMethod]
         public void AgendaRemoveDoisContatosPorNumero()
         {   
 
