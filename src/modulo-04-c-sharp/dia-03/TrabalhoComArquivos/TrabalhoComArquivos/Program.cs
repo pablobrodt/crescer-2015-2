0a94d4970461f5bf07db7ccd31d739b27309d19c
Ben-hur Santos Ott <ben-hur@outlook.com>
2015-10-28 15:21:51 -0200

    Incluindo códigos de exemplo para trabalho com arquivos.

--- src/modulo-04-c-sharp/dia-03/TrabalhoComArquivos/TrabalhoComArquivos/Program.cs
+++ src/modulo-04-c-sharp/dia-03/TrabalhoComArquivos/TrabalhoComArquivos/Program.cs
@@ -4,13 +4,98 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace TrabalhoComArquivos
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            //double salario = 10;
+
+            //string texto = String.Format("meu salario eh {0}, {0}", salario);
+            //Console.WriteLine(texto);
+
+            //Console.WriteLine(DateTime.Now);
+            //Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy"));
+            /*
+            string texto = String.Format("minha data de pagamento eh {0:dd-MM-yyyy}", DateTime.Now);
+            Console.WriteLine(texto);
+            Console.Read();
+            */
+        }
+
+        /*
+        static void Main(string[] args)
+        {
+            string caminhoArquivo = @"C:\Users\Ben-hur\Desktop\arquivos\jogos.xml";
+
+            XElement xmlJogos = XElement.Load(caminhoArquivo);
+
+            foreach (XElement jogo in xmlJogos.Elements("jogo"))
+            {
+                XElement nome = jogo.Element("nome");
+                Console.WriteLine(jogo.Attribute("id"));
+                Console.WriteLine(nome.Value);
+            }
+
+            Console.Read();
+        }
+        */
+
+        /*
+        static void Main(string[] args)
+        {
+            string caminhoArquivo = @"C:\Users\Ben-hur\Desktop\arquivos\meu-arquivo.txt";
+
+            using (var reader = new StreamReader(caminhoArquivo))
+            {
+                reader.ReadLine();
+
+            }
+
+            //try
+            //{
+            //    ;
+            //}
+            //finally
+            //{
+            //    if(reader != null)
+            //    {
+            //        reader.Dispose();
+            //    }
+            //}
+            Console.Read();
+        }
+        */
+        /*
+        static void Main(string[] args)
+        {
+            string caminhoArquivo = @"C:\Users\Ben-hur\Desktop\arquivos\meu-arquivo.txt";
+            var arquivoPrincipal = File.Open(caminhoArquivo, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+
+            var reader = new StreamReader(caminhoArquivo);
+            var leitura = reader.ReadToEnd();
+
+            Console.Read();
+        }*/
+
+        /*
+        static void Main(string[] args)
+        {
+            string caminhoArquivo = @"C:\Users\Ben-hur\Desktop\arquivos\meu-arquivo.txt";
+
+            var writer = new StreamWriter(caminhoArquivo, true);
+
+            writer.WriteLine("didi must die");
 
 
+            writer.Close();
+
+            Console.Read();
+        }
+        */
 
         /*
         static void Main(string[] args)
