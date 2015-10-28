d1a89b1f6de6d974d3a402d8c7e64b58e435a860
Pablo Brodt <pablobrodt@gmail.com>
2015-10-28 19:39:02 -0200

    feat(Jogo): adicionar classe

---
+++ src/modulo-04-c-sharp/dia-03/TrabalhoLocadora/Locadora/Locadora.Dominio/Jogo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Dominio
+{
+    public class Jogo
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public string Categoria { get; set; }
+        public bool Disponibilidade { get; set; }
+
+        public Jogo(string nome, double preco, string categoria, bool disponibilidade)
+        {
+            this.Nome = nome;
+            this.Preco = preco;
+            this.Categoria = categoria;
+            this.Disponibilidade = disponibilidade;
+        }
+    }
+}
