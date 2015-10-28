6a28304b517c63f703bf7e88455b3885f9140806
Pablo Brodt <pablobrodt@gmail.com>
2015-10-28 19:39:26 -0200

    feat(GerenciadorDeJogos): adicionar classe

---
+++ src/modulo-04-c-sharp/dia-03/TrabalhoLocadora/Locadora/Locadora.Dominio/GerenciadorDeJogos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace Locadora.Dominio
+{
+    public class GerenciadorDeJogos
+    {
+        public List<Jogo> jogos = new List<Jogo>();
+
+        public GerenciadorDeJogos()
+        {
+            this.jogos = new List<Jogo>();
+        }
+
+        public Jogo[] pesquisarPorNome(string nomeJogo)
+        {
+
+            XElement[] jogosPesquisados = XElement.Load(BaseDeDados.Caminho).Elements("jogo").Where(jogo => jogo.Element("nome").Value.Contains(nomeJogo)).ToArray();
+
+            foreach (XElement jogo in jogosPesquisados)
+            {
+                string nome = jogo.Element("nome").Value;
+                double preco = XmlConvert.ToDouble(jogo.Element("preco").Value);
+                string categoria = jogo.Element("categoria").Value;
+                bool disponibilidade = true;
+
+                var objJogo = new Jogo(nome, preco, categoria, disponibilidade);
+
+                jogos.Add(objJogo);
+            }
+            
+            return this.jogos.ToArray();
+        }
+
+    }
+}
---
+++ src/modulo-04-c-sharp/dia-03/TrabalhoLocadora/Locadora/Locadora.Tests/GerenciadorDeJogosTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Locadora.Dominio;
+
+namespace Locadora.Tests
+{
+    [TestClass]
+    public class GerenciadorDeJogosTest
+    {
+        [TestMethod]
+        public void BuscaPorNomeChronoTriggerRetornaChronoTrigger()
+        {
+            GerenciadorDeJogos gerenciador = new GerenciadorDeJogos();
+
+            Jogo jogo = gerenciador.pesquisarPorNome("Chrono Trigger")[0];
+
+            Assert.AreEqual(jogo.Nome, "Chrono Trigger");
+            Assert.AreEqual(jogo.Preco, 60);
+            Assert.AreEqual(jogo.Categoria, "RPG");
+
+        }
+
+        [TestMethod]
+        public void BuscaPorNomeFinalRetornaFinalFantasyEFinalFight()
+        {
+            GerenciadorDeJogos gerenciador = new GerenciadorDeJogos();
+
+            Jogo[] jogo = gerenciador.pesquisarPorNome("Final");
+
+            Assert.AreEqual("Final Fight", jogo[0].Nome);
+            Assert.AreEqual(12, jogo[0].Preco);
+            Assert.AreEqual("AVENTURA", jogo[0].Categoria);
+
+            Assert.AreEqual("Final Fantasy VI", jogo[1].Nome);
+            Assert.AreEqual(30.5, jogo[1].Preco);
+            Assert.AreEqual("RPG", jogo[1].Categoria);
+        }
+    }
+}
