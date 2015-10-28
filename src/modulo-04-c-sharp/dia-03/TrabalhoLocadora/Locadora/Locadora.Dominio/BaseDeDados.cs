679d1b57e6f09290e4f9b4961f14a9a6365c847a
Pablo Brodt <pablobrodt@gmail.com>
2015-10-28 19:38:31 -0200

    feat(BaseDeDados): adicionar classe

---
+++ src/modulo-04-c-sharp/dia-03/TrabalhoLocadora/Locadora/Locadora.Dominio/BaseDeDados.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Dominio
+{
+    class BaseDeDados
+    {
+        public static string Caminho = @"C:\locadora\game_store.xml";
+    }
+}
