869f7cafafeeca9c71a408e29506db23eeb34584
Pablo Brodt <pablobrodt@gmail.com>
2015-10-30 02:30:16 -0200

    feat(Categoria): adicionar enum

---
+++ src/modulo-04-c-sharp/dia-03/TrabalhoLocadora/Locadora/Locadora.Dominio/Categoria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Dominio
+{
+    public enum Categoria
+    {   
+        Aventura = 0,
+        Corrida = 1,
+        Luta = 2,
+        Rpg = 3,
+        Esporte = 4
+    }
+}
