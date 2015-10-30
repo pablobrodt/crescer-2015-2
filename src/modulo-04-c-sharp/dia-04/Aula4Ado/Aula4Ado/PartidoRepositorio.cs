ac48a54935d2e558e8142167a8e57b7eb4130036
Ben-hur Santos Ott <ben-hur@outlook.com>
2015-10-30 17:32:15 -0200

    chore(ProjADO): Adicionando projeto para aula de ADO.

---
+++ src/modulo-04-c-sharp/dia-04/Aula4Ado/Aula4Ado/PartidoRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula4Ado
+{
+    class PartidoRepositorio : IRepositorio<Partido>
+    {
+        public Partido BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
