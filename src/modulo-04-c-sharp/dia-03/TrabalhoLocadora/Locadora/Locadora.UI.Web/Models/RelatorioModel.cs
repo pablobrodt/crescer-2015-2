3fb2d982d134e427527a820890ce5dc569ab3087
Pablo Brodt <pablobrodt@gmail.com>
2015-11-03 23:42:01 -0200

    feat(RelatorioModel): adicionar model

---
+++ src/modulo-04-c-sharp/dia-03/TrabalhoLocadora/Locadora/Locadora.UI.Web/Models/RelatorioModel.cs
@@ -0,0 +1,23 @@
+﻿using Locadora.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Locadora.UI.Web.Models
+{
+    public class RelatorioModel
+    {
+        private GerenciadorDeRelatorios gerenciador;
+
+        public RelatorioModel()
+        {
+            this.gerenciador = new GerenciadorDeRelatorios();
+        }
+
+        public RelatorioJogosDisponiveis RelatorioJogosDisponiveis()
+        {
+            return gerenciador.ObterRelatorioJogosDisponiveis();
+        }
+    }
+}
