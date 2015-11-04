290c374491c8a02c50da8488a1b4365690f1532f
Pablo Brodt <pablobrodt@gmail.com>
2015-11-03 23:38:36 -0200

    feat(RelatorioJogosdisponiveis): adicionar classe

---
+++ src/modulo-04-c-sharp/dia-03/TrabalhoLocadora/Locadora/Locadora.Dominio/RelatorioJogosDisponiveis.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Dominio
+{
+    public class RelatorioJogosDisponiveis : Relatorio
+    {   
+        public int QuantidadeJogosDisponiveis { get; private set; }
+
+        public RelatorioJogosDisponiveis()
+        {
+            Data = DataAtual.ToString("dd/MM/yyyy");
+            Hora = DataAtual.ToString("HH:mm:ss");
+            Titulo = "Relatório de jogos";
+
+            GerenciadorDeJogos gj = new GerenciadorDeJogos();
+
+            this.Jogos = gj.BuscarJogosDisponiveis();
+
+            ObterTotalJogosDisponiveis();
+        }
+
+        private void ObterTotalJogosDisponiveis()
+        {
+            this.QuantidadeJogosDisponiveis = this.Jogos.Count();
+        }
+    }
+}
