77305f66482118057a259e695071d1ba1ad0593f
Pablo Brodt <pablobrodt@gmail.com>
2015-11-04 17:56:01 -0200

    feat(Model): adicionar model

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/Models/RelatorioModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Locadora.Web.MVC.Models
+{
+    public class RelatorioModel
+    {
+        public List<JogoModel> Jogos { get; set; }
+
+        public int QuantidadeTotalJogos { get; set; }
+        public decimal ValorMedioJogos { get; set; }
+        public string JogoMaisCaro { get; set; }
+        public string JogoMaisBarato { get; set; }
+
+        public RelatorioModel()
+        {
+            Jogos = new List<JogoModel>();
+        }
+    }
+}
