2e870260e07e98b8dca6d5f4dea62d322808fd0e
Pablo Brodt <pablobrodt@gmail.com>
2015-11-04 17:50:50 -0200

    feat(JogoModel): adicionar model

--- src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/Models/JogoModel.cs
+++ src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/Models/JogoModel.cs
@@ -6,10 +6,9 @@ using System.Web;
 namespace Locadora.Web.MVC.Models
 {
     public class JogoModel
-    {
-        public int Id { get; set; }
+    {   
         public string Nome { get; set; }
-        public decimal Preco { get; set; }
         public string Categoria { get; set; }
+        public decimal Preco { get; set; }
     }
 }
