57cdfaedc0b9e11e6c7a4a554f05e0929aa9dbe2
Pablo Brodt <pablobrodt@gmail.com>
2015-11-03 23:41:40 -0200

    feat(RelatorioController): adicionar controller

---
+++ src/modulo-04-c-sharp/dia-03/TrabalhoLocadora/Locadora/Locadora.UI.Web/Controllers/RelatorioController.cs
@@ -0,0 +1,22 @@
+﻿using Locadora.UI.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Locadora.UI.Web.Controllers
+{
+    public class RelatorioController : Controller
+    {
+        // GET: Relatorio
+        public ActionResult JogosDisponiveis()
+        {
+            var model = new RelatorioModel();
+
+            model.RelatorioJogosDisponiveis();
+
+            return View(model);
+        }
+    }
+}
