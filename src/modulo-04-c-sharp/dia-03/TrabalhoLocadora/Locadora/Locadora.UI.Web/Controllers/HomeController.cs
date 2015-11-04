babcbd7a6f8ae4af88c19c3fc95d6b26d90bd239
Pablo Brodt <pablobrodt@gmail.com>
2015-11-03 23:41:22 -0200

    feat(Homecontroller): adicionar controller

---
+++ src/modulo-04-c-sharp/dia-03/TrabalhoLocadora/Locadora/Locadora.UI.Web/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Locadora.UI.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Locadora.UI.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Listar()
+        {
+            var model = new JogoModel();
+
+            return View(model);
+        }
+    }
+}
