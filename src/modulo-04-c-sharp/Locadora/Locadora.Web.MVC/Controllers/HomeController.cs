0292e9951139c8c8283f12a4cad1a8f12c96c2b9
Fabrício Rissetto <fabriciorissetto@gmail.com>
2015-11-05 14:44:23 -0200

    refactor: alteração dos links e imagens para usarem Helpers do MVC

--- src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/Controllers/HomeController.cs
+++ src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 namespace Locadora.Web.MVC.Controllers
 {
     public class HomeController : Controller
-    {
+    {        
         public ActionResult Index()
         {
             return View();
--- src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/Controllers/RelatorioController.cs
+++ src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/Controllers/RelatorioController.cs
@@ -1,4 +1,5 @@
-﻿using Locadora.Web.MVC.Models;
+﻿using Locadora.Dominio.Repositorio;
+using Locadora.Web.MVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,16 +10,31 @@ namespace Locadora.Web.MVC.Controllers
 {
     public class RelatorioController : Controller
     {
+        private IJogoRepositorio repositorio = new Locadora.Repositorio.ADO.JogoRepositorio();
+        
+        public ActionResult JogosFiltradosPorId(int id)
+        {
+            return View();
+        }
+
+        public ActionResult JogosFiltradosPorNome(string nome)
+        {
+            return View();
+        }
+
         public ActionResult JogosDisponiveis()
         {
-            var model = new List<JogoModel>()
+            var model = new RelatorioModel();
+
+            foreach (var jogo in repositorio.BuscarTodos())
             {
-                new JogoModel() { Id = 1, Nome = "Teste", Preco = 9.9m, Categoria="RPG" },
-                new JogoModel() { Id = 1, Nome = "dasd", Preco = 5m, Categoria="Aventura" },
-                new JogoModel() { Id = 1, Nome = "dasda", Preco = 10m, Categoria="RPG" }
-            };
+                var jogoModel = new JogoModel() { Nome = jogo.Nome, Preco = jogo.Preco, Categoria = jogo.Categoria.ToString() };
+                model.Jogos.Add(jogoModel);
+            }
+
+            model.QuantidadeTotalDeJogos = model.Jogos.Count();
 
             return View(model);
-        }
+        }        
     }
 }
