c6e98653374375b8ddfaf18f82399640201a52b1
Pablo Brodt <pablobrodt@gmail.com>
2015-11-08 02:56:20 -0200

    refactor(JogoModel): remover propriedades não utilizadas

--- src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/Models/JogoModel.cs
+++ src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/Models/JogoModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Locadora.Dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,16 +12,8 @@ namespace Locadora.Web.MVC.Models
 
         public string Nome { get; set; }
 
-        public string Categoria { get; set; }
+        public Categoria Categoria { get; set; }
 
         public decimal Preco { get; set; }
-
-        public string Descricao { get; set; }
-
-        public string Selo { get; set; }
-
-        public string Imagem { get; set; }
-
-        public string Video { get; set; }
     }
 }
