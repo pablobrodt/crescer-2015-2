92f0a152267c6648bd9b53903a8026638cfb3f05
Pablo Brodt <pablobrodt@gmail.com>
2015-11-08 02:56:52 -0200

    feat(DetalheJogoModel): adicionar model

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/Models/DetalhejogoModel.cs
@@ -0,0 +1,27 @@
+﻿using Locadora.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Locadora.Web.MVC.Models
+{
+    public class DetalheJogoModel
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public Categoria Categoria { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public string Descricao { get; set; }
+
+        public Selo Selo { get; set; }
+
+        public string Imagem { get; set; }
+
+        public string Video { get; set; }
+    }
+}
