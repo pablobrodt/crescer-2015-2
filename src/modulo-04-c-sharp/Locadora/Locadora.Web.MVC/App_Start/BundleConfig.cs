7bdc8bbace28d56738de3fc913e3a48522fc950b
Pablo Brodt <pablobrodt@gmail.com>
2015-11-09 11:22:26 -0200

    feat(Exercicio 2): adicionada solução do exercício 2

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Optimization;
+
+namespace Locadora.Web.MVC.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/validation").Include(
+                        "~/Scripts/jquery.validate.js",
+                        "~/Scripts/jquery.validate.unobtrusive.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                        "~/Content/bootstrap.css",
+                        "~/Content/site.css"));
+        }
+    }
+}
