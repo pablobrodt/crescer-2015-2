7c846e52d7f1f365050dc29d7b2528df197fd948
Pablo Brodt <pablobrodt@gmail.com>
2015-11-20 17:49:37 -0200

    feat(Lavanderia): adicionar projeto

---
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/HomeController.java
@@ -0,0 +1,20 @@
+package br.com.cwi.crescer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+
+        String mensagem = "Bem Vindo, Lavanderia Crescer";
+
+        model.addAttribute("mensagem", mensagem);
+
+        return "index";
+    }
+
+}
