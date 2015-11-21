5b4f49a70728c6de4eb0fbc38c1ce6ea652bb31e
Pablo Brodt <pablobrodt@gmail.com>
2015-11-21 21:51:03 -0200

    feat(Cidade): adicionar annotations

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/domain/Cidade.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/domain/Cidade.java
@@ -1,11 +1,33 @@
 package br.com.cwi.crescer.lavanderia.domain;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Cidade")
+@SequenceGenerator(name = Cidade.SEQUENCE_NAME,
+sequenceName = Cidade.SEQUENCE_NAME)
 public class Cidade {
 	
+	public static final String SEQUENCE_NAME = "SEQ_Cidade";
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQUENCE_NAME)
+	@Column(name = "IDCidade")
 	private Long idCidade;
 	
+	@Column(name="Nome")
+	@Basic(optional = false)
 	private String nome;
 	
+	@Column(name="UF")
+	@Basic(optional = false)
 	private String uf;
 	
 	public Long getIdCidade() {
