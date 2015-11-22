62e09a64beaef18dbf1fd062a39f93fe80586400
Pablo Brodt <pablobrodt@gmail.com>
2015-11-22 17:47:37 -0200

    feat(ProdutoController): adicionar metodo

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/ProdutoController.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/ProdutoController.java
@@ -2,7 +2,10 @@ package br.com.cwi.crescer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.cwi.crescer.lavanderia.domain.Produto;
 import br.com.cwi.crescer.lavanderia.service.ProdutoService;
 
 @Controller
@@ -16,4 +19,14 @@ public class ProdutoController {
 		this.ps = produtoService;
 	}
 	
+	@RequestMapping("/Produto/")
+	public String index(Model model){
+		
+		Produto produto = this.ps.findById(1L);
+		
+		model.addAttribute("IDProduto", produto.getIdProduto());
+		model.addAttribute("Valor", produto.getValor());
+		
+		return "produto/index";
+	}
 }
