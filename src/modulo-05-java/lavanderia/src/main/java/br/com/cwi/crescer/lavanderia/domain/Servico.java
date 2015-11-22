b880f5a86e3ebe6803251372db39bd7aaf311872
Pablo Brodt <pablobrodt@gmail.com>
2015-11-21 22:35:51 -0200

    feat(Servico): adicionar annotations

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/domain/Servico.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/domain/Servico.java
@@ -1,14 +1,29 @@
 package br.com.cwi.crescer.lavanderia.domain;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "Servico")
+@SequenceGenerator(name = Servico.SEQUENCE_NAME,
+sequenceName = Servico.SEQUENCE_NAME)
 public class Servico {
-
-	private Long idServico;
+	
+	public static final String SEQUENCE_NAME = "SEQ_Servico";
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQUENCE_NAME)
+	private int idServico;
 	private String descricao;
 	
-	public Long getIdServico() {
+	
+	public int getIdServico() {
 		return idServico;
 	}
-	public void setIdServico(Long idServico) {
+	public void setIdServico(int idServico) {
 		this.idServico = idServico;
 	}
 	public String getDescricao() {
