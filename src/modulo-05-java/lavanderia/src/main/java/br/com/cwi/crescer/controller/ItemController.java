7b80d25684a83bf4a0e60e42cb3415126589b84c
Pablo Brodt <pablobrodt@gmail.com>
2015-11-22 18:40:28 -0200

    feat(ItemController): adicionar metodo

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/ItemController.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/ItemController.java
@@ -2,7 +2,10 @@ package br.com.cwi.crescer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.cwi.crescer.lavanderia.domain.Item;
 import br.com.cwi.crescer.lavanderia.service.ItemService;
 
 @Controller
@@ -16,4 +19,17 @@ public class ItemController {
 		this.is = itemService;
 	}
 	
+	@RequestMapping("/Item/")
+	public String index(Model model){
+		
+		Item item = this.is.findById(1L);
+		
+		model.addAttribute("IDItem", item.getIdItem());
+		model.addAttribute("Peso", item.getPeso());
+		model.addAttribute("ValorUnitario", item.getValorUnitario());
+		model.addAttribute("ValorDesconto", item.getValorDesconto());
+		model.addAttribute("ValorTotal", item.getValorTotal());
+		
+		return "item/index";
+	}
 }
