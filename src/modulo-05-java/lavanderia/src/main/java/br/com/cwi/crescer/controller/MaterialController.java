271826e68f9caa086f9c889c57d2190323deaabb
Pablo Brodt <pablobrodt@gmail.com>
2015-11-22 17:31:44 -0200

    feat(MaterialController): adicionar metodo

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/MaterialController.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/MaterialController.java
@@ -2,7 +2,10 @@ package br.com.cwi.crescer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.cwi.crescer.lavanderia.domain.Material;
 import br.com.cwi.crescer.lavanderia.service.MaterialService;
 
 @Controller
@@ -14,6 +17,16 @@ public class MaterialController {
 	public MaterialController(MaterialService materialService){
 		super();
 		this.ms = materialService;
+	}
+	
+	@RequestMapping("/Material/")
+	public String index(Model model){
+		
+		Material material = this.ms.findById(1L);
+		
+		model.addAttribute("Descricao", material.getDescricao());
+		
+		return "material/index";
 	}
 	
 }
