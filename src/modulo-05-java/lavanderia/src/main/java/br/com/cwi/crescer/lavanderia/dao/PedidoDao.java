223fa09540fbdd2dfa7d52944b0a60d1e7fb205c
Pablo Brodt <pablobrodt@gmail.com>
2015-11-22 14:27:08 -0200

    feat(PedidoDao): implementar interface

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/dao/PedidoDao.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/dao/PedidoDao.java
@@ -1,5 +1,21 @@
 package br.com.cwi.crescer.lavanderia.dao;
 
-public class PedidoDao {
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.cwi.crescer.lavanderia.domain.Pedido;
+
+@Repository
+public class PedidoDao implements IBaseDao<Pedido> {
+	
+	@PersistenceContext
+	private EntityManager em;
+	
+	@Override
+	public Pedido findById(Long id) {
+		return em.find(Pedido.class, id);
+	}
 
 }
