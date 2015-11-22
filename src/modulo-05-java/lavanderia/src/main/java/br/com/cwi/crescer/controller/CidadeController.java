b74162c2980115ce29ec24eb369b03b51eed5a16
Pablo Brodt <pablobrodt@gmail.com>
2015-11-22 02:09:55 -0200

    feat(CidadeController): adicionar controller e view

---
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/CidadeController.java
@@ -0,0 +1,33 @@
+package br.com.cwi.crescer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.cwi.crescer.lavanderia.domain.Cidade;
+import br.com.cwi.crescer.lavanderia.service.CidadeService;
+
+@Controller
+public class CidadeController {
+	
+	private CidadeService cs;
+	
+	@Autowired
+	public CidadeController(CidadeService cidadeService){
+		super();
+		this.cs = cidadeService;
+	}
+	
+	@RequestMapping("/Cidade/")
+	public String index(Model model){
+		
+		Cidade c = cs.buscarCidade(1L);
+		
+		model.addAttribute("Cidade", c.getNome());
+		model.addAttribute("UF", c.getUf());
+		
+		return "cidade/index";
+	}
+	
+}
