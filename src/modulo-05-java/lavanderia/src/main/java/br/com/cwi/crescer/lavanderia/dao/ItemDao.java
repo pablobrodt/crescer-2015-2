ba6ef08284a1805da4cb2dac6a6f1a3dc0fe219a
Pablo Brodt <pablobrodt@gmail.com>
2015-11-22 14:23:20 -0200

    feat(ItemDao): implementar interface

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/dao/ItemDao.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/dao/ItemDao.java
@@ -1,5 +1,23 @@
 package br.com.cwi.crescer.lavanderia.dao;
 
-public class ItemDao {
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
+import org.springframework.stereotype.Repository;
+
+import br.com.cwi.crescer.lavanderia.domain.Item;
+
+@Repository
+public class ItemDao implements IBaseDao<Item>{
+	
+	@PersistenceContext
+	private EntityManager em;
+	
+	@Override
+	public Item findById(Long id) {
+		return em.find(Item.class, id);
+	}
+	
+	
+	
 }
