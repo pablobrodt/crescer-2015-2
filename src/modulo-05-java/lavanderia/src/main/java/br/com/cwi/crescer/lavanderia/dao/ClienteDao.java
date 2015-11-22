dd37807216e5c8f5ce8bd8c28146418beea3a256
Pablo Brodt <pablobrodt@gmail.com>
2015-11-22 12:58:39 -0200

    feat(ClienteDao): implementar interface

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/dao/ClienteDao.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/dao/ClienteDao.java
@@ -1,5 +1,20 @@
 package br.com.cwi.crescer.lavanderia.dao;
 
-public class ClienteDao {
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
+import org.springframework.stereotype.Repository;
+
+import br.com.cwi.crescer.lavanderia.domain.Cliente;
+
+@Repository
+public class ClienteDao implements IBaseDao<Cliente>{
+	
+	@PersistenceContext
+	private EntityManager em;
+
+	@Override
+	public Cliente findById(Long id) {
+		return em.find(Cliente.class, id);
+	}
 }
