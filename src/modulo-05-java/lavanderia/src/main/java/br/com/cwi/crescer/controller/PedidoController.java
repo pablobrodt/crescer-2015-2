cabf22720aa2775254bb1c59950eab93b237924b
Pablo Brodt <pablobrodt@gmail.com>
2015-11-22 17:32:57 -0200

    feat(PedidoController): adicionar metodo

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/PedidoController.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/PedidoController.java
@@ -2,7 +2,10 @@ package br.com.cwi.crescer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.cwi.crescer.lavanderia.domain.Pedido;
 import br.com.cwi.crescer.lavanderia.service.PedidoService;
 
 @Controller
@@ -16,4 +19,16 @@ public class PedidoController {
 		this.ps = pedidoService;
 	}
 	
+	@RequestMapping("/Pedido/")
+	public String index(Model model){
+		
+		Pedido pedido = this.ps.findById(1L);
+		
+		model.addAttribute("IDPedido", pedido.getIdPedido());
+		model.addAttribute("DataInclusao", pedido.getDataInclusao());
+		model.addAttribute("DataEntrega", pedido.getDataEntrega());
+		model.addAttribute("Valor", pedido.getValor());
+		
+		return "pedido/index";
+	}
 }
