d2fddbfdbf3c4eb3cccdc92de987de0afe7e5cef
Pablo Brodt <pablobrodt@gmail.com>
2015-11-21 22:35:11 -0200

    feat(Produto): adicionar annotations

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/domain/Produto.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/domain/Produto.java
@@ -1,30 +1,44 @@
 package br.com.cwi.crescer.lavanderia.domain;
 
 import java.math.BigDecimal;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "Produto")
+@SequenceGenerator(name = Produto.SEQUENCE_NAME,
+sequenceName = Produto.SEQUENCE_NAME)
 public class Produto {
-
-	private Long idProduto;
-	private Long idServico;
-	private Long idMaterial;
+	
+	public static final String SEQUENCE_NAME = "SEQ_Produto";
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQUENCE_NAME)
+	private int idProduto;
+	private int idServico;
+	private int idMaterial;
 	private BigDecimal valor;
 	
-	public Long getIdProduto() {
+	public int getIdProduto() {
 		return idProduto;
 	}
-	public void setIdProduto(Long idProduto) {
+	public void setIdProduto(int idProduto) {
 		this.idProduto = idProduto;
 	}
-	public Long getIdServico() {
+	public int getIdServico() {
 		return idServico;
 	}
-	public void setIdServico(Long idServico) {
+	public void setIdServico(int idServico) {
 		this.idServico = idServico;
 	}
-	public Long getIdMaterial() {
+	public int getIdMaterial() {
 		return idMaterial;
 	}
-	public void setIdMaterial(Long idMaterial) {
+	public void setIdMaterial(int idMaterial) {
 		this.idMaterial = idMaterial;
 	}
 	public BigDecimal getValor() {
