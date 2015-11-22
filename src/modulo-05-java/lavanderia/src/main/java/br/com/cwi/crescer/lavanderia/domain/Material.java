b00055202921c2d9f9680c1a2b5447d15d171e29
Pablo Brodt <pablobrodt@gmail.com>
2015-11-21 22:32:41 -0200

    feat(Material): adicionar annotations

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/domain/Material.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/domain/Material.java
@@ -1,14 +1,28 @@
 package br.com.cwi.crescer.lavanderia.domain;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "Material")
+@SequenceGenerator(name = Material.SEQUENCE_NAME,
+sequenceName = Material.SEQUENCE_NAME)
 public class Material {
-
-	private Long idMaterial;
+	
+	public static final String SEQUENCE_NAME = "SEQ_Material";
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQUENCE_NAME)
+	private int idMaterial;
 	private String descricao;
 	
-	public Long getIdMaterial() {
+	public int getIdMaterial() {
 		return idMaterial;
 	}
-	public void setIdMaterial(Long idMaterial) {
+	public void setIdMaterial(int idMaterial) {
 		this.idMaterial = idMaterial;
 	}
 	public String getDescricao() {
