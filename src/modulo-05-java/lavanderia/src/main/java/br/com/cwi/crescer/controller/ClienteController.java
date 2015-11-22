1e23c53ad4f66d09a3452246bf098201309d063e
Pablo Brodt <pablobrodt@gmail.com>
2015-11-22 14:44:38 -0200

    feat(ClienteController):adicionar metodo

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/ClienteController.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/ClienteController.java
@@ -2,7 +2,10 @@ package br.com.cwi.crescer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.cwi.crescer.lavanderia.domain.Cliente;
 import br.com.cwi.crescer.lavanderia.service.ClienteService;
 
 @Controller
@@ -14,6 +17,20 @@ public class ClienteController {
 	public ClienteController(ClienteService clienteService){
 		super();
 		this.cs = clienteService;
+	}
+	
+	@RequestMapping("/Cliente/")
+	public String index(Model model){
+		
+		Cliente cliente = this.cs.findById(1L);
+		
+		model.addAttribute("Nome", cliente.getNome());
+		model.addAttribute("Cpf", cliente.getCpf());
+		model.addAttribute("Endereco", cliente.getEndereco());
+		model.addAttribute("Bairro", cliente.getBairro());
+		model.addAttribute("Email", cliente.getEmail());
+		
+		return "cliente/index";
 	}
 	
 }
