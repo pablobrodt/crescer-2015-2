7c03d7b263ee93a2535263c1e4a6b6eaacbeab7a
Pablo Brodt <pablobrodt@gmail.com>
2015-11-22 02:09:37 -0200

    feat(CidadeServide): adicionar service

---
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/service/CidadeService.java
@@ -0,0 +1,23 @@
+package br.com.cwi.crescer.lavanderia.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.cwi.crescer.lavanderia.dao.CidadeDao;
+import br.com.cwi.crescer.lavanderia.domain.Cidade;
+
+@Service
+public class CidadeService {
+	
+	private CidadeDao cidadeDao;
+	
+	@Autowired
+	public CidadeService(CidadeDao cidadeDao){
+		super();
+		this.cidadeDao = cidadeDao;
+	}
+	
+	public Cidade buscarCidade(Long id){
+		return this.cidadeDao.findById(id);
+	}
+}	
