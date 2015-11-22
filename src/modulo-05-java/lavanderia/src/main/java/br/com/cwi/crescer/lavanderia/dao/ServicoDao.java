40ded194fe67c3f87f53b914db2918c76d32225b
Pablo Brodt <pablobrodt@gmail.com>
2015-11-22 14:30:30 -0200

    feat(ServicoDao): implementar interface

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/dao/ServicoDao.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/dao/ServicoDao.java
@@ -1,5 +1,21 @@
 package br.com.cwi.crescer.lavanderia.dao;
 
-public class ServicoDao {
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
+import org.springframework.stereotype.Repository;
+
+import br.com.cwi.crescer.lavanderia.domain.Servico;
+
+@Repository
+public class ServicoDao implements IBaseDao<Servico> {
+	
+	@PersistenceContext
+	private EntityManager em;
+	
+	@Override
+	public Servico findById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
