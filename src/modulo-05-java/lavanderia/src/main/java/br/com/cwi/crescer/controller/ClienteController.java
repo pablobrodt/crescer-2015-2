e8d99a4489976bd676dd558271f96f6081d97bd1
Pablo Brodt <pablobrodt@gmail.com>
2015-11-22 12:40:11 -0200

    feat(Controller): adicionar controllers

---
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/ClienteController.java
@@ -0,0 +1,12 @@
+package br.com.cwi.crescer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import br.com.cwi.crescer.lavanderia.service.ClienteService;
+
+
+@Controller
+public class ClienteController {
+	
+}
