89d8c4c194f7ced804444d8872c98ab958dfd574
Pablo Brodt <pablobrodt@gmail.com>
2015-11-27 02:54:22 -0200

    feat(Lavanderia): adicionar autenticação

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/LavanderiaApplication.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/LavanderiaApplication.java
@@ -2,11 +2,18 @@ package br.com.cwi.crescer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class LavanderiaApplication {
+public class LavanderiaApplication extends WebMvcConfigurerAdapter{
 
     public static void main(String[] args) {
         SpringApplication.run(LavanderiaApplication.class, args);
+    }
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
     }
 }
--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/ClienteController.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/ClienteController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -44,11 +45,13 @@ public class ClienteController {
 		return new ModelAndView("cliente/exibe", "cliente", ClienteMapper.toDTO(this.clienteService.findById(id)));
 	}
 	
+	@PreAuthorize("hasRole('ADMIN')")
 	@RequestMapping(path = "/Editar/{id}", method = RequestMethod.GET)
 	public ModelAndView viewEditar(@PathVariable("id") Long id){
 		return new ModelAndView("cliente/editar", "cliente", ClienteMapper.toDTO(clienteService.findById(id)));
 	}
 	
+	@PreAuthorize("hasRole('ADMIN')")
 	@RequestMapping(path = "/Editar", method = RequestMethod.POST)
 	public ModelAndView editar(@Valid @ModelAttribute("cliente") ClienteDTO dto,
 	        					BindingResult result,
