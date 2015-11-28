9c27a338dfb6a612d4f35d1f75e520b65cec69ea
Pablo Brodt <pablobrodt@gmail.com>
2015-11-28 09:52:15 -0200

    feat(ClienteUrl): adicionar classe

---
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/urlmapper/ClienteUrl.java
@@ -0,0 +1,11 @@
+package br.com.cwi.crescer.controller.urlmapper;
+
+public class ClienteUrl {
+	
+	public static final String BASE = "/Clientes";
+	public static final String LISTA = BASE + "/Listar";
+	public static final String NOVO = BASE + "/Novo";
+	public static final String DETALHE = BASE + "/Detalhes";
+	public static final String EDITA = BASE + "/Editar";
+	public static final String REMOVE = BASE + "/Remover";
+}
