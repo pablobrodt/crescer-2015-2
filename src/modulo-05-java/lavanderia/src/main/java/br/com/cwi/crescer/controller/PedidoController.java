f9fc987d82ede387fb6f739824e09966f9869614
Pablo Brodt <pablobrodt@gmail.com>
2015-11-28 18:08:37 -0200

    refactor(PedidoController): alterar mapeamento

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/PedidoController.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/PedidoController.java
@@ -2,14 +2,13 @@ package br.com.cwi.crescer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import br.com.cwi.crescer.lavanderia.domain.Pedido;
-import br.com.cwi.crescer.lavanderia.service.PedidoService;
+import br.com.cwi.crescer.controller.urlmapper.PedidoUrl;
+import br.com.cwi.crescer.lavanderia.service.pedido.PedidoService;
 
 @Controller
-@RequestMapping("/Pedidos")
+@RequestMapping(PedidoUrl.BASE)
 public class PedidoController {
 	
 	private PedidoService ps;
@@ -20,15 +19,5 @@ public class PedidoController {
 	}
 	
 	
-	public String index(Model model){
-		
-		Pedido pedido = this.ps.findById(1L);
-		
-		model.addAttribute("IDPedido", pedido.getIdPedido());
-		model.addAttribute("DataInclusao", pedido.getDataInclusao());
-		model.addAttribute("DataEntrega", pedido.getDataEntrega());
-		model.addAttribute("Valor", pedido.getValor());
-		
-		return "pedido/index";
-	}
+
 }
