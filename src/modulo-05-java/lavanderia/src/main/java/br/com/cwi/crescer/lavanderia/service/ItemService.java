dd22b5053d590d6ec1fe73e421582dd20a8030ee
Pablo Brodt <pablobrodt@gmail.com>
2015-11-28 19:00:42 -0200

    feat(ItemService): adicionar metodo

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/service/ItemService.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/service/ItemService.java
@@ -1,5 +1,7 @@
 package br.com.cwi.crescer.lavanderia.service;
 
+import java.math.BigDecimal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,4 +22,12 @@ public class ItemService {
 	public Item findById(Long id){
 		return this.itemDao.findById(id);
 	}
+	
+	public void calcularValorTotalItem(Item entity){
+		BigDecimal total = new BigDecimal(0);
+		total = total.add(entity.getValorUnitario().multiply(entity.getPeso()));
+		
+		entity.setValorTotal(total);
+	}
+	
 }
