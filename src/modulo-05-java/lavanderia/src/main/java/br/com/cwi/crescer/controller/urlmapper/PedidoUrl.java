8a0218d7b930ba63fbaf1c9e840ac9def2445047
Pablo Brodt <pablobrodt@gmail.com>
2015-11-29 02:21:11 -0200

    feat(PedidoUrl): adicionar constante

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/urlmapper/PedidoUrl.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/urlmapper/PedidoUrl.java
@@ -2,6 +2,7 @@ package br.com.cwi.crescer.controller.urlmapper;
 
 public class PedidoUrl {
 
-	public static final String BASE = "/Pedidos";	
+	public static final String BASE = "/Pedidos";
+	public static final String NOVO = BASE + "/Novo";
 	
 }
