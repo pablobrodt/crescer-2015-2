243014807885ff48fd3b8e01ddb4688e3e083041
Pablo Brodt <pablobrodt@gmail.com>
2015-11-30 09:11:51 -0200

    feat(ProdutoController): adicionar mapeamento para requisições via ajax

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/ProdutoController.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/ProdutoController.java
@@ -1,32 +1,40 @@
 package br.com.cwi.crescer.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.cwi.crescer.lavanderia.domain.Produto;
+import br.com.cwi.crescer.lavanderia.dto.MaterialDTO;
+import br.com.cwi.crescer.lavanderia.dto.ProdutoDTO;
 import br.com.cwi.crescer.lavanderia.service.ProdutoService;
 
 @Controller
 @RequestMapping("/Produtos")
 public class ProdutoController {
 
-	private ProdutoService ps;
+	private ProdutoService produtoService;
 	
 	@Autowired
 	public ProdutoController(ProdutoService produtoService){
-		this.ps = produtoService;
+		this.produtoService = produtoService;
 	}
 	
+	@ResponseBody
+	@RequestMapping(path = "/rest/{id}")
+	public List<ProdutoDTO> buscarPorServico(@PathVariable("id") Long id) {
+	    return produtoService.findByServico(id);
+	}
 	
-	public String index(Model model){
-		
-		Produto produto = this.ps.findById(1L);
-		
-		model.addAttribute("IDProduto", produto.getIdProduto());
-		model.addAttribute("Valor", produto.getValor());
-		
-		return "produto/index";
+	@ResponseBody
+	@RequestMapping(path = "/rest/{idServico}/{idMaterial}")
+	public ProdutoDTO buscarPorServico(@PathVariable("idServico") Long idServico, 
+											@PathVariable("idMaterial") Long idMaterial) {
+	    return produtoService.findByServicoMaterial(idServico, idMaterial);
 	}
 }
