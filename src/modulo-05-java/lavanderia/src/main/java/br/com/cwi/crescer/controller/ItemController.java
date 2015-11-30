f48d1807f05732297db82c25f22475c77e820a97
Pablo Brodt <pablobrodt@gmail.com>
2015-11-30 09:11:15 -0200

    feat(ItemController): adicionar metodos

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/ItemController.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/controller/ItemController.java
@@ -1,34 +1,66 @@
 package br.com.cwi.crescer.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-import br.com.cwi.crescer.lavanderia.domain.Item;
+import br.com.cwi.crescer.controller.urlmapper.ItemUrl;
+import br.com.cwi.crescer.lavanderia.dto.ItemDTO;
+import br.com.cwi.crescer.lavanderia.dto.MaterialDTO;
+import br.com.cwi.crescer.lavanderia.dto.PedidoDTO;
+import br.com.cwi.crescer.lavanderia.dto.ServicoDTO;
+import br.com.cwi.crescer.lavanderia.mapper.CidadeMapper;
 import br.com.cwi.crescer.lavanderia.service.ItemService;
+import br.com.cwi.crescer.lavanderia.service.MaterialService;
+import br.com.cwi.crescer.lavanderia.service.ServicoService;
 
 @Controller
-@RequestMapping("/Itens")
+@RequestMapping(ItemUrl.BASE)
 public class ItemController {
 
 	private ItemService is;
+	private ServicoService servicoService;
+	private MaterialService materialService;
+	
+	private ArrayList<ItemDTO> itens;
 	
 	@Autowired
-	public ItemController(ItemService itemService){
+	public ItemController(ItemService itemService, ServicoService servicoService, MaterialService materialService){
 		this.is = itemService;
+		this.servicoService = servicoService;
+		this.materialService = materialService;
+		this.itens = new ArrayList<>();
 	}
 	
-	public String index(Model model){
-		
-		Item item = this.is.findById(1L);
-		
-		model.addAttribute("IDItem", item.getIdItem());
-		model.addAttribute("Peso", item.getPeso());
-		model.addAttribute("ValorUnitario", item.getValorUnitario());
-		model.addAttribute("ValorDesconto", item.getValorDesconto());
-		model.addAttribute("ValorTotal", item.getValorTotal());
-		
-		return "item/index";
+	@RequestMapping("/Novo")
+	public ModelAndView viewNovo(){
+		return new ModelAndView("item/novo", "item", new ItemDTO());
 	}
+	
+	@RequestMapping(path = "/Novo", method = RequestMethod.POST)
+	public ModelAndView adicionarItem(HttpServletRequest request, @ModelAttribute("item") ItemDTO dto){
+		PedidoDTO pedido = (PedidoDTO)request.getSession().getAttribute("pedido");
+		
+		
+		pedido.addItem(dto);
+		return new ModelAndView("pedido/novo", "pedido", pedido);
+	}
+	
+	@ModelAttribute("servicos")
+    public List<ServicoDTO> comboServicos() {
+        return servicoService.findAll();
+    }
+	
+	@ModelAttribute("materiais")
+    public List<MaterialDTO> comboMateriais() {
+        return materialService.findAll();
+    }
 }
